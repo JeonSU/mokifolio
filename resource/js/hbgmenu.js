a74638c7670b389e846c73b358c191cf990444cc
JeonSU <wjs7438@naver.com>
2019-05-28 14:59:12 +0900

    1
    1

---
+++ resource/js/hbgmenu.js
@@ -0,0 +1,17 @@
+$('document').ready(function () {
+      var Closed = false;
+
+      $('.hamburger').click(function () {
+        if (Closed == true) {
+          $(this).removeClass('open');
+          $(this).addClass('closed');
+          Closed = false;
+        } else {               
+          $(this).removeClass('closed');
+          $(this).addClass('open');
+          Closed = true;
+        }
+      });
+});
+
+
